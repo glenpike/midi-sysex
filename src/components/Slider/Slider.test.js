19c3dca6d2c098b876a6c37d0bb148a713afb83b
Glen Pike <github@glenpike.co.uk>
2021-02-03 18:03:37 +0000

    Tweaking CSS for slider thumb and group fieldset / legend

--- src/components/Slider/Slider.test.js
+++ src/components/Slider/Slider.test.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import { act } from 'react-dom/test-utils'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, screen } from '@testing-library/react'
 
 import Slider from './Slider'
 
 const handler = jest.fn()
 
 describe('Slider', () => {
-	let container
-
-	const props = {
+	const defaultProps = {
 		id: 'test-id',
 		name: 'test-name',
 		label: 'test-label',
@@ -19,42 +16,39 @@ describe('Slider', () => {
 		disabled: false,
 	}
 
-	beforeEach(() => {
-		act(() => {
-			;({ container } = render(<Slider {...props} />))
-		})
-	})
-
 	afterEach(() => {
-		container = null
 		handler.mockReset
 	})
 
-	it('renders the with the correct label', () => {
-		expect(container.querySelector('label').textContent).toContain(props.label)
+	const getSlider = (props = defaultProps) => {
+		render(<Slider {...props} />)
+		return screen.getByLabelText(new RegExp(defaultProps.label))
+	}
+
+	it('renders', () => {
+		expect(getSlider()).toBeTruthy()
 	})
 
 	it('sets a default value', () => {
-		expect(container.querySelector('input').value).toBe('0')
+		expect(getSlider().value).toBe('0')
 	})
 
 	it('I can set a value', () => {
-		const testProps = { ...props, value: 20 }
-		act(() => {
-			;({ container } = render(<Slider {...testProps} />))
-		})
-		expect(container.querySelector('input').value).toBe('20')
+		const testProps = { ...defaultProps, value: 20 }
+		expect(getSlider(testProps).value).toBe('20')
 	})
 
 	it('it sets min and max values', () => {
-		expect(container.querySelector('input').min).toBe(`${props.range[0]}`)
-		expect(container.querySelector('input').max).toBe(`${props.range[1]}`)
+		const slider = getSlider()
+
+		expect(slider.min).toBe(`${defaultProps.range[0]}`)
+		expect(slider.max).toBe(`${defaultProps.range[1]}`)
 	})
 
 	it('calls the handler with the right value	', () => {
-		const input = container.querySelector('input')
+		const slider = getSlider()
 		const value = '20'
-		fireEvent.change(input, { target: { value: value } })
-		expect(handler).toHaveBeenCalledWith(props.address, value)
+		fireEvent.change(slider, { target: { value: value } })
+		expect(handler).toHaveBeenCalledWith(defaultProps.address, value)
 	})
 })
