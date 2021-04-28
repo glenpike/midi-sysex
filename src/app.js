ff53eae18ccfeff0f589102c131773906900c256
Glen Pike <github@glenpike.co.uk>
2021-04-28 11:24:21 +0100

    Remove app testing.
    - We can't seem to use the 'concrete' provider in a test - wrapper test
    has to use a provider with injected values that are mocked.
    Ideally  we could use the real WebMidiContextProvider because WebMidi is mocked
    but if we try to render anything with this, it hangs tests - could be stuck in
    a rendering loop?

--- src/app.js
+++ src/app.js
@@ -1,33 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { WebMidiContextProvider } from './contexts/WebMidiContext.js'
 import "./app.scss";
 
-import MidiSelect from './components/MidiSelect/MidiSelect.js'
-import GroupControl from './components/GroupControl/GroupControl.js'
-import { controlConfig, efxConfig } from './controls.js'
-import EFXControls from './components/EFXControls/EFXControls.js'
+import Wrapper from './wrapper.js';
 
-// class App extends Component {
 const App = () => {
-	
-		console.log('App')
-	
 		return (
 			<WebMidiContextProvider>
-				<div className="control__bar">
-					<h1 className="control__heading"> Midi Sysex Controls </h1>
-					<MidiSelect />
-				</div>
-				
-				<div className="control__layout">
-					{ controlConfig.map((control, index) =>
-						<GroupControl key={index} sliderConfig={control} />
-					)}
-					<EFXControls efxConfig={efxConfig} />
-				</div>
+				<Wrapper/>
 			</WebMidiContextProvider>
 		)
 	}
-// }
 
 export default App
